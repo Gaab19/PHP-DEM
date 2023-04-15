97531abe19437acce7c6d51207491f2f86b7efbd
Gaab19 <128101666+Gaab19@users.noreply.github.com>
2023-04-14 21:19:03 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Meu Site</title>
+    <title>Document</title>
 </head>
 <body>
-  <center>
-      <form id="login" action="aulawhile.php">
-        <input type="submit" name="Entrar" value="Entrar">
-  <center>
-
-
+    <h1>Cadastre-se Abaixo!<h1>
+    <form id= "form.login" action= "login.php" method= "POST">
+        login:<input type= "text" name="login"><br>
+        senha:<input type= "password" name="senha"><br>
+        <input type="submit" name="entrar" value="entrar">
 </body>
-</html>
+</html><html>
+
+
+
