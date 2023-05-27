51ee8afe6ebd24a4b5bfb07ffc99b014572d57e8
Gaab19 <128101666+Gaab19@users.noreply.github.com>
2023-05-26 21:24:01 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$conexao= mysqli_connect('localhost', 'root', '','projetoa3','3306');
+if (!$conexao){
+    die("ERRO DE CONEXÃO :( ");
+    
+}else{
+
+    echo "VOCÊ ESTA CONECTADO :D ";
+
+
+}
+
+
+
+
+
+?>
